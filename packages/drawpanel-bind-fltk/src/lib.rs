869f7f80f184ab01b89f681191e9c1844eab1a72
WumaCoder <15804854160@163.com>
2022-07-24 23:49:12 +0800

    feat(bind-fltk): Implement the interface with hookevent

--- packages/drawpanel-bind-fltk/src/lib.rs
+++ packages/drawpanel-bind-fltk/src/lib.rs
@@ -1,25 +1,46 @@
 use std::{cell::RefCell, rc::Rc};
 
 use drawpanel_core::{
-    binder::{Binder, Draw, DrawCircleOpts, DrawLineOpts, DrawRectOpts, EventType},
+    binder::{Binder, Draw, DrawCircleOpts, DrawLineOpts, DrawRectOpts, EventType, HookEvent},
     drawpanel::Drawpanel,
+    elem::{text::Text, Elem},
 };
 use fltk::{
     app,
     draw::{self, LineStyle},
-    enums::{Color, Event, FrameType},
+    enums::{Align, CallbackTrigger, Color, Event, Font, FrameType},
     frame::Frame,
-    prelude::{WidgetBase, WidgetExt},
+    input,
+    prelude::{GroupExt, InputExt, WidgetBase, WidgetExt},
+    window,
 };
-use geo::coord;
+use geo::{coord, Coordinate};
 
 pub struct FltkBinder {
     frame: Frame,
+    input: input::MultilineInput,
 }
 
 impl FltkBinder {
-    pub fn new(frame: Frame) -> Self {
-        FltkBinder { frame }
+    pub fn new(frame: Frame, mut win: window::DoubleWindow) -> Self {
+        let mut input = input::MultilineInput::default();
+        win.add(&input);
+        input.set_frame(FrameType::NoBox);
+        input.set_align(Align::Center);
+
+        input.handle({
+            let mut frame = frame.clone();
+            move |inp, e| match e {
+                Event::KeyDown => {
+                    inp.redraw();
+                    frame.redraw();
+                    true
+                }
+                _ => false,
+            }
+        });
+
+        FltkBinder { frame, input }
     }
 }
 
@@ -33,6 +54,7 @@ impl Binder for FltkBinder {
         });
 
         self.frame.handle({
+            let mut input = self.input.clone();
             let drawpanel = Rc::clone(&drawpanel);
             move |frm, e| {
                 let (x, y) = app::event_coords();
@@ -45,6 +67,7 @@ impl Binder for FltkBinder {
                         (*drawpanel)
                             .borrow_mut()
                             .trigger_event(EventType::Move, mouse_coord);
+                        input.redraw();
                         frm.redraw();
                         true
                     }
@@ -73,6 +96,12 @@ impl Binder for FltkBinder {
                 }
             }
         });
+    }
+
+    fn hook_event(&self) -> Box<dyn drawpanel_core::binder::HookEvent> {
+        Box::new(FltkHookEvent {
+            input: self.input.clone(),
+        })
     }
 }
 
@@ -113,5 +142,47 @@ impl Draw for FltkDraw {
             size - 1,
             Color::from_hex(opts.fill_color),
         );
+    }
+
+    fn draw_text(&self, opts: drawpanel_core::binder::DrawTextOpts) {
+        draw::set_draw_color(Color::from_hex(opts.font_color));
+        draw::set_font(Font::Screen, opts.font_size as i32);
+        draw::draw_text2(
+            &opts.content,
+            opts.left_top_coord.x as i32,
+            opts.left_top_coord.y as i32,
+            opts.width as i32,
+            opts.height as i32,
+            Align::Center,
+        )
+    }
+}
+
+struct FltkHookEvent {
+    input: input::MultilineInput,
+}
+
+impl HookEvent for FltkHookEvent {
+    fn before_create(&mut self, elem: &mut Box<dyn Elem>) {}
+    fn creating(&mut self, elem: &mut Box<dyn Elem>, mouse_coord: Coordinate) {
+        if elem.need_input() {
+            let ver = elem.get_vertex();
+            let left_top = ver.get(0).unwrap();
+            let right_bottom = ver.get(2).unwrap();
+            self.input
+                .set_pos((left_top.x + 3.) as i32, (left_top.y + 3.) as i32);
+            self.input.set_size(
+                (right_bottom.x - left_top.x - 6.) as i32,
+                (right_bottom.y - left_top.y - 6.) as i32,
+            );
+        }
+    }
+    fn after_create(&mut self, elem: &mut Box<dyn Elem>) {
+        if elem.need_input() {
+            let value = self.input.value();
+            elem.set_content(&value);
+            self.input.set_size(0, 0);
+            self.input.set_value("");
+        }
     }
 }
